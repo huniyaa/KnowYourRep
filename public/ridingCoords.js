3417109da47529dea4a9fc7f748fd41c4282f505
Your Name <your.email@example.com>
2026-03-26 13:00:28 -0400

    commit

--- public/ridingCoords.js
+++ public/ridingCoords.js
@@ -2,4 +2,5 @@ const ridingCoords = {
   "Toronto Centre": { lat: 43.654, lng: -79.377 },
   "Vancouver East": { lat: 49.282, lng: -123.065 },
   "Edmonton Manning": { lat: 53.571, lng: -113.39 },
+  // ... all 338 ridings
 };
